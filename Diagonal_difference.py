e926794dbdaf26f02abcb8d6c5b4c44811722ea1
Harsh Trivedi <152944798+Harshtrivedi456@users.noreply.github.com>
2025-07-27 11:07:35 +0530

    Create Diagonal_difference.py

---
+++ Diagonal_difference.py
@@ -0,0 +1,15 @@
+n = int(input())
+matrix = []
+
+for _ in range(n):
+    row = list(map(int, input().split()))
+    matrix.append(row)
+
+primary_sum = 0
+secondary_sum = 0
+
+for i in range(n):
+    primary_sum += matrix[i][i]
+    secondary_sum += matrix[i][n - i - 1]
+
+print(abs(primary_sum - secondary_sum))
