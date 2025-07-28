e3d28957dd65cb1558119b2174e44917c0254dcf
Harsh Trivedi <152944798+Harshtrivedi456@users.noreply.github.com>
2025-07-28 16:16:38 +0530

    Create divisible_sumpairs.py

---
+++ divisible_sumpairs.py
@@ -0,0 +1,14 @@
+def divisibleSumPairs(n, k, ar):
+    count = 0
+    for i in range(n):
+        for j in range(i + 1, n):
+            if (ar[i] + ar[j]) % k == 0:
+                count += 1
+    return count
+
+# Input
+n, k = map(int, input().split())
+ar = list(map(int, input().split()))
+
+# Output
+print(divisibleSumPairs(n, k, ar))
