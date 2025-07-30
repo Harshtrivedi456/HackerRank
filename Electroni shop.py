3062056d70b82a5113c5405e52a85906dda4aad5
Harsh Trivedi <152944798+Harshtrivedi456@users.noreply.github.com>
2025-07-30 08:31:11 +0530

    Create Electroni shop.py

---
+++ Electroni shop.py
@@ -0,0 +1,16 @@
+def getMoneySpent(keyboards, drives, b):
+    max_spend = -1  # Assume can't buy anything
+    for k in keyboards:
+        for d in drives:
+            total = k + d
+            if total <= b and total > max_spend:
+                max_spend = total
+    return max_spend
+
+# Read all input correctly
+b, n, m = map(int, input().split())           # e.g. 10 2 3
+keyboards = list(map(int, input().split()))   # e.g. 3 1
+drives = list(map(int, input().split()))      # e.g. 5 2 8
+
+# Output
+print(getMoneySpent(keyboards, drives, b))
