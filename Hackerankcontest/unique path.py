d8da81cac47a045b7780fd2d696fee21ba028237
@H4rSh <152944798+Harshtrivedi456@users.noreply.github.com>
2025-11-08 10:32:13 +0530

    Update unique path.py

--- Hackerankcontest/unique path.py
+++ Hackerankcontest/unique path.py
@@ -1,1 +1,20 @@
 
+a, b = map(int, input().split())
+mat = [list(map(int, input().split())) for _ in range(a)]
+
+if mat[0][0] == 1 or mat[a-1][b-1] == 1:
+    print(0)
+else:
+    ab = [[0]*b for _ in range(a)]
+    ab[0][0] = 1
+
+    for i in range(a):
+        for j in range(b):
+            if mat[i][j] == 1:
+                ab[i][j] = 0
+            else:
+                if i > 0:
+                    ab[i][j] += ab[i-1][j]
+                if j > 0:
+                    ab[i][j] += ab[i][j-1]
+    print(ab[a-1][b-1])
