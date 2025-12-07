c36d7dc043a5ea69c66bd2b1c567ff32201a0e29
@H4rSh <152944798+Harshtrivedi456@users.noreply.github.com>
2025-12-07 18:30:53 +0530

    Create binarystrong.py

---
+++ binarystrong.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def beautifulBinaryString(b):
+    count = 0
+    i = 0
+    while i < len(b) - 2:
+        if b[i:i+3] == "010":
+            count += 1
+            i += 3   # skip this pattern
+        else:
+            i += 1
+    return count
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    b = input().strip()
+    print(beautifulBinaryString(b))
